6b13e0d883cf28ef59e66a751cbb39792da7b006
蒋志庆 <jiangzhiqing4@h-partners.com>
2026-01-06 09:37:17 +0800

    client UT
    Signed-off-by: 蒋志庆 <jiangzhiqing4@h-partners.com>

--- test/unittest/common/midi_test_common.h
+++ test/unittest/common/midi_test_common.h
@@ -14,7 +14,6 @@ public:
     MOCK_METHOD(std::vector<DeviceInformation>, GetRegisteredDevices, (), (override));
     MOCK_METHOD(int32_t, OpenDevice, (int64_t deviceId), (override));
     MOCK_METHOD(int32_t, CloseDevice, (int64_t deviceId), (override));
-    // Note: Use exact signature match for OpenInputPort/CloseInputPort
     MOCK_METHOD(int32_t, OpenInputPort, (int64_t deviceId, size_t portIndex, UmpInputCallback cb), (override));
     MOCK_METHOD(int32_t, CloseInputPort, (int64_t deviceId, size_t portIndex), (override));
     MOCK_METHOD(int32_t, HanleUmpInput, (int64_t deviceId, size_t portIndex, MidiEventInner list), (override));
@@ -25,7 +24,6 @@ public:
     MOCK_METHOD(void, NotifyDeviceChange, (DeviceChangeType change, (std::map<int32_t, std::string>) deviceInfo), (override));
     MOCK_METHOD(void, NotifyError, (int32_t code), (override));
 };
-
 }
 }
 #endif 
